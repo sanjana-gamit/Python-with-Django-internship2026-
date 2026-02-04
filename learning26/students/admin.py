eac53d6562a1a46b06827ce4181d0d6c209341db
Sanjana Gamit <sanjanabengamit123@gmail.com>
2026-02-05 01:24:09 +0530

    feat: Implement initial Django models for Student, Product, StudentResult, and StudentCourse, including migrations, admin registration, and settings configuration.

--- learning26/students/admin.py
+++ learning26/students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student,Product,StudentResult,StudentCourse
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Product)
+admin.site.register(StudentResult)
+admin.site.register(StudentCourse)
--- learning26/students/models.py
+++ learning26/students/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+#python class
+
+#parent class model
+#create table student(studentName varchar(100))
+class Student(models.Model):
+    studentName = models.CharField(max_length=100)
+    studentAge = models.IntegerField()
+    studentCity = models.CharField(max_length=40)
+    studentEmail = models.EmailField(null=True)
+
+#meta class
+    class Meta:
+        db_table = "student"
+
+class Product(models.Model):
+    productName = models.CharField(max_length=100)
+    productPrice = models.IntegerField()
+    productDescription = models.TextField()
+    productStock = models.PositiveIntegerField()
+    productColor = models.CharField(null=True)
+    productStatus = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "product"
+
+
+class StudentResult(models.Model):
+    studentId = models.IntegerField()
+    studentName = models.CharField(max_length=100)
+    studentMarks = models.IntegerField()
+    studentGrade = models.CharField(max_length=10)
+    studentStatus = models.BooleanField(default=True)
+    
+    class Meta:
+        db_table = "studentresult"
+
+class StudentCourse(models.Model):
+    studentId = models.IntegerField()
+    studentName = models.CharField(max_length=100)
+    studentCourse = models.CharField(max_length=100)
+    studentStatus = models.BooleanField(default=True)
+    
+    class Meta:
+        db_table = "studentcourse"
+    
+
+
+    
+
